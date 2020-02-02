de9853becf34425621e5f35f0e9c4666f89cd6ca
Ben Kwok <ben.kwok.cy@gmail.com>
2020-02-01 22:28:23 -0800

    Previous commit failed to include all changed files

---
+++ test.py
@@ -0,0 +1,14 @@
+from app import logic
+from testing import mocks
+
+def avoid_wall_down():
+    assert logic.getMove(mocks.moveDown) == "down"
+
+def avoid_wall_up():
+    assert logic.getMove(mocks.moveUp) == "up"
+
+def avoid_wall_left():
+    assert logic.getMove(mocks.moveLeft) == "left"
+
+def avoid_wall_right():
+    assert logic.getMove(mocks.moveRight) == "right"
